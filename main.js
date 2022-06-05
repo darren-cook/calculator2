60cc5adb5e563418e9ccf8fd022217474ba79224
darren-cook <darren.j.cook@uconn.edu>
2022-06-05 14:14:54 -0400

    history button and history list

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";console.log(1+2)})();
+(()=>{"use strict";const t=document.getElementById("history-button"),s=document.getElementById("history-list");t.addEventListener("click",(()=>{console.log("test"),s.classList.toggle("history-list-closed"),s.classList.toggle("history-list-open")}))})();
